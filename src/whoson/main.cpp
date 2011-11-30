61c911fdf887e1484ec796bfccbe8c6ff607712d
Anders Lövgren <andlov@nowise.se>
2011-11-30 00:58:30 +0000

    Wrap main application behavour in a class. Sort of java-style ;-).

--- src/whoson/main.cpp
+++ src/whoson/main.cpp
@@ -9,18 +9,70 @@
 # include <stdlib.h>
 #endif
 
+#include "whoson.hpp"     // library
 #include "options.hpp"
+
+class Application
+{
+public:
+	Application(Options *options) : opts(options) 
+	{
+		if(getenv("WHOSON_DEBUG")) {
+			opts->Debug = atoi("WHOSON_DEBUG") > 0;
+		}
+		if(getenv("WHOSON_VERBOSE")) {
+			opts->Verbose = atoi("WHOSON_VERBOSE");
+		}
+		if(getenv("WHOSON_SOAP_ENDPOINT")) {
+			proxy.soap_endpoint = getenv("WHOSON_SOAP_ENDPOINT");
+		}
+	}
+	
+	void Parse(int argc, char **argv)
+	{
+		try {
+			opts->Parse(argc, argv);
+		} catch(Options::ArgumentException exception) {
+			std::cerr << exception.msg << std::endl;
+			exit(1);
+		}
+	}
+
+	void Process()
+	{	
+		WhosOn::LogonEventAdapter adapter(&proxy);
+	
+		try {
+			switch(opts->GetReason()) {
+			case Options::Login:
+				adapter.Add(opts->GetFilter());
+				break;
+			case Options::Logout:
+				adapter.Close(opts->GetFilter());
+				break;
+			case Options::List:
+				break;
+			case Options::Unknown:
+				break;
+			}
+		} catch(WhosOn::SoapServiceException exception) {
+			exception.Write(std::cerr);
+			exit(1);
+		}
+	}
+	
+private:
+	SoapServiceProxy proxy;
+	Options *opts;
+};
 
 int main(int argc, char **argv)
 {
 	Options options;
+	Application app(&options);
 	
-	try {
-		options.Parse(argc, argv);
-	} catch(Options::ArgumentException exception) {
-		// std::cerr << exception.msg << std::endl;
-		exit(1);
-	}
+	app.Parse(argc, argv);
+	app.Process();
 	
 	return 0;
 }
