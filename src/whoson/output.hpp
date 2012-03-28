a0f64b603590c4d154edc4a3d78ed2e203d79467
Anders Lövgren <andlov@nowise.se>
2012-03-28 15:36:13 +0000

    Adding copyright header to source code files.

--- src/whoson/output.hpp
+++ src/whoson/output.hpp
@@ -1,3 +1,22 @@
+// WhosOn Linux - Centralized Logon Accounting (WhosOn Program Suite).
+// 
+// Copyright (C) 2011-2012 Anders Lövgren, Computing Department at BMC,
+// Uppsala Biomedical Centre, Uppsala University.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
 #ifndef __OUTPUT_HPP__
 #define __OUTPUT_HPP__
 
